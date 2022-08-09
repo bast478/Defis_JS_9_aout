f3d86838bcf8053cd3c547dbaa08bfb5426a567b
Babsybis <babsybis@hotmail.fr>
2022-08-09 09:40:12 +0200

    fait

---
+++ defi_3/script.js
@@ -0,0 +1,16 @@
+const centre = document.querySelector('.centre');
+
+let i=0;
+centre.addEventListener('click', function (){
+    i++;
+    console.log(i);
+    if(i % 10 === 0){
+        centre.classList.add('centre');
+    }else if (i % 2 === 0){
+        centre.classList.remove('impair');
+        centre.classList.add('pair');
+    }else{
+        centre.classList.remove('pair');
+        centre.classList.add('impair');
+    }
+});
